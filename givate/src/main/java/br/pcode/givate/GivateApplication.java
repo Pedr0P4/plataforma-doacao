ac49f5633bb25d3b86db21be02a01c3eeabef743
Pedro Paulo <pedropaulo062011@gmail.com>
2026-07-08 17:08:12 -0300

    :construction: feat(backend): Começo da refatoração

---
+++ givate/src/main/java/br/pcode/givate/GivateApplication.java
@@ -0,0 +1,13 @@
+package br.pcode.givate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivateApplication.class, args);
+	}
+
+}
---
+++ givate/src/test/java/br/pcode/givate/GivateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pcode.givate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
